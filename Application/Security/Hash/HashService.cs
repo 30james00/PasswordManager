57a3de1c35a9d4bff8a8994b717fca1579e4d928
30james00 <48206982+30james00@users.noreply.github.com>
2022-10-19 12:12:35 +0200

    Implement HashService.cs

--- Application/Security/Hash/HashService.cs
+++ Application/Security/Hash/HashService.cs
@@ -1,14 +1,28 @@
+using System.Security.Cryptography;
+
 namespace PasswordManager.Application.Security.Hash;
 
 public class HashService : IHashService
 {
     public string CalculateSHA512(string text)
     {
-        throw new NotImplementedException();
+        //split string into bytes
+        var bytes = System.Text.Encoding.UTF8.GetBytes(text);
+
+        using var sha512 = SHA512.Create();
+        var hashValue = sha512.ComputeHash(bytes);
+
+        //join separate bytes into string
+        return hashValue.Aggregate("", (current, hashByte) => current + $"{hashByte:x2}");
     }
 
     public string CalculateHMAC(string text, string key)
     {
-        throw new NotImplementedException();
+        var textBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        var keyBytes = System.Text.Encoding.UTF8.GetBytes(key);
+
+        using var hmacSha512 = new HMACSHA512(keyBytes);
+        var hashValue = hmacSha512.ComputeHash(textBytes);
+        return hashValue.Aggregate("", (current, hashByte) => current + $"{hashByte:x2}");
     }
 }

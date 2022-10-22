751417c967e678aa3244f515385a0067e30edf4d
30james00 <48206982+30james00@users.noreply.github.com>
2022-10-22 15:55:43 +0200

    Migrate not-nullable Salt

---
+++ Infrastructure/Migrations/20221022135335_Not nullable Salt.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class NotnullableSalt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WebAdress",
+                table: "SavedPasswords",
+                newName: "WebAddress");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Salt",
+                table: "Accounts",
+                type: "TEXT",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WebAddress",
+                table: "SavedPasswords",
+                newName: "WebAdress");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Salt",
+                table: "Accounts",
+                type: "TEXT",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 512);
+        }
+    }
+}
